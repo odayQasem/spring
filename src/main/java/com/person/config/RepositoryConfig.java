17642681176d2512731094923347c7cabb70393b
Oday qasem <oday.qasim@gmail.com>
2019-03-20 14:10:07 +0200

    add id to resopnse

--- src/main/java/com/person/config/RepositoryConfig.java
+++ src/main/java/com/person/config/RepositoryConfig.java
@@ -1,4 +1,5 @@
 package com.person.config;
+import com.person.model.Person;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -7,6 +8,6 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapt
 public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-//        config.exposeIdsFor(Person.class);
+        config.exposeIdsFor(Person.class); //to retun id with the response
     }
 }
